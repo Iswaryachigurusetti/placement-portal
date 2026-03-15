ef811d104deb5b7ebc6eeaae6c4e396ae9e35b34
Iswarya2006 <161280986+Iswaryachigurusetti@users.noreply.github.com>
2026-03-15 21:25:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -90,6 +90,7 @@ app.post("/api/login", async (req, res) => {
 
 /* Start Server */
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
 });
